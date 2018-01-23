5f49d34e79731586e3cc2210832af8aa215f196b
AbhilashJN <abhilashjn107@gmail.com>
2018-01-23 11:29:22 +0530

    2 test cases with valid input for calcStrike passed

--- bowling.js
+++ bowling.js
@@ -16,6 +16,27 @@ function score(frameArray)
 	let modArray = parseFrames(frameArray);
 	
 
+	for(let i=0;i<modArray.length;i++)
+	{
+		if(frameArray[i].length===1) // is a strike
+		{
+			score+=10;
+			score+= calcStrike(frameArray,i+1);    //add next two scores
+			
+		}
+		else if (frameArray[i].length===2)
+		{
+			if(frameArray[i][0]+frameArray[i][1] === 10)   //is a spare
+			{
+				score+=10;
+				score+=frameArray[i+1][0];
+			}
+		}
+        
+		else{                                                    //is open
+			score += (frameArray[i][0]+frameArray[i][1]);
+		}
+	}
 
 	return score;
 }
@@ -42,4 +63,31 @@ function parseFrames(frameArray)
 
 
 
-module.exports = { parseFrames: parseFrames , score: score};
+function calcStrike(parsedArray , begin)
+{
+	console.log(parsedArray,begin);
+	console.log(parsedArray[begin] , parsedArray[begin].length);
+	let currScore =0;
+	if(parsedArray[begin].length===1)
+	{
+        
+		console.log("1" , parsedArray[begin]);
+		currScore += 10;
+		currScore += parsedArray[begin+1][0];
+	}
+	else if(parsedArray[begin].length===2)
+	{
+		currScore += parsedArray[begin][0];
+		currScore += parsedArray[begin][1];
+        
+	}
+    
+	return currScore;
+}
+
+
+module.exports = { 
+	parseFrames: parseFrames,
+	calcStrike: calcStrike,
+	score: score
+};
--- bowling.test.js
+++ bowling.test.js
@@ -2,6 +2,10 @@ let myfuncs = require("./bowling");
 
 let sampleInp;
 let sampleParsed;
+let sampleBegin;
+let sampleBegin2;
+
+
 
 sampleInp = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
 sampleParsed = [[3,6],[3,6],[3,6],[3,6],[3,6],[3,6],[3,6],[3,6],[3,6],[3,6]];
@@ -18,4 +22,26 @@ test("For given input the score should be 90",  ()=>{
 
 
 
+
+
+
+
+sampleInp = [10, 3, 6, 2, 8, 3, 6, 3, 7, 3, 6, 3, 6, 10, 3, 6, 3, 6];
+sampleParsed = [[10],[3,6],[2,8],[3,6],[3,7],[3,6],[3,6],[10],[3,6],[3,6]];
+sampleBegin = 1;
+
+test("For given input the strike score should be 19" , ()=>{
+	expect(myfuncs.calcStrike(sampleParsed,sampleBegin)).toBe(9);
+});
+
+
+
+
+sampleInp = [10, 3, 6, 2, 8, 3, 6, 3, 7, 3, 6, 3, 6, 10, 2, 5, 3, 6];
+sampleParsed = [[10],[3,6],[2,8],[3,6],[3,7],[3,6],[3,6],[10],[2,5],[3,6]];
+sampleBegin2 = 8;
+
+test("For given input the strike score should be 19" , ()=>{
+	expect(myfuncs.calcStrike(sampleParsed,sampleBegin2)).toBe(7);
+});
 
